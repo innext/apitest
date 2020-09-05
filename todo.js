4b148d2617538f387daec4cbd8ee39934cb7095b
Inioluwa <57904303+innext@users.noreply.github.com>
2020-09-05 21:30:53 +0100

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+
+title : {
+        type: String,
+        index: true,
+        trim: true,
+        require: true,
+        unique: true
+        },
+category : {
+        type: String,
+        trim: true,
+        require: true
+        },
+note : {
+        type: String,
+        trim: true,
+        require:true
+        }
+
+});
+
+const Todo = module.exports = mongoose.model("Todo", todoSchema);
+
+module.exports.sendAllTodo = (allTodo, limit) => {
+        Todo.find(allTodo).lean().limit(limit);
+        }
+
+module.exports.newTodo = (todo, callback) => {
+        todo.save(callback);
+        }
